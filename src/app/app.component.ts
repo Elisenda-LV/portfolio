080a2f3f95aa295c8803cf2c1dd52351c8f0cfc8
Elisenda-LV <elisenda.lv@gmail.com>
2024-10-28 18:34:27 +0100

    translate config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
-import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, BreadcrumbComponent],
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    HeaderComponent,
+    FooterComponent,
+
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'portfolio';
+  selectedLanguage = 'en';
+
+  constructor(private translateService: TranslateService){}
+
+  onLanguageChange() {
+    this.translateService.use(this.selectedLanguage)
+  }
 }
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
