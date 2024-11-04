44adb4f8b0772541f03dded6c0d5fafdcd75a68a
Elisenda-LV <elisenda.lv@gmail.com>
2024-11-04 17:43:48 +0100

    i18n added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
 import { FormsModule } from '@angular/forms';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -12,12 +13,24 @@ import { FormsModule } from '@angular/forms';
     FormsModule,
     HeaderComponent,
     FooterComponent,
+    TranslateModule
 
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'portfolio';
+  title = 'elv portfolio';
+  selectedLanguage = 'es';
+
+  constructor(private translateService: TranslateService){
+    this.translateService.setDefaultLang(this.selectedLanguage);
+    this.translateService.use(this.selectedLanguage);
+  }
+
+  onLanguageChange(newLanguage: string) {
+    this.selectedLanguage = newLanguage;
+    this.translateService.use(this.selectedLanguage);
+  }
 
 }
